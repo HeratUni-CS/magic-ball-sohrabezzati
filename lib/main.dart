319f7bea0edede8d4734c9471ba8c669847eae59
sohrabezzati <s.ezzati.30.dec.1999@gmail.com>
2023-05-02 19:17:55 +0430

    scaffold created

--- lib/main.dart
+++ lib/main.dart
@@ -11,6 +11,13 @@ void main() {
 class BallPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return scaffold(
+      backgrounColor: Colors.blue,
+      appBar: AppBar(
+        backgroundColor: Colors.blue[900],
+        title: Text('Ask Me Anything'),
+      ),
+      body: Container(),
+    );
   }
 }
