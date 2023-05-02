bc2bf34ec0555b94b4f99131a9c4515132b14773
sohrabezzati <s.ezzati.30.dec.1999@gmail.com>
2023-05-02 19:08:18 +0430

    BallPage created

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,14 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(
     const MaterialApp(
-      home: null,
+      home: BallPage,
     ),
   );
+}
+
+class BallPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
 }
